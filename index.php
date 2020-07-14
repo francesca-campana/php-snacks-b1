5da8d5a7fa6f03fb27bd8e5864dafaa661f2484d
francesca-campana <franx3rox@gmail.com>
2020-07-14 14:43:05 +0200

    snack 1-2

---
+++ index.php
@@ -0,0 +1,75 @@
+<!-- SNACK N°1 -->
+<?php
+
+// Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite
+// di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra
+// di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+// Stampiamo a schermo tutte le partite con questo schema:
+//  Olimpia Milano - Cantù | 55-60
+
+// $array_matches = [
+//   [
+//     'ospite' => 'Milano',
+//     'casa' => 'Lecce',
+//     'punti_casa'=> 4,
+//     'punti_ospite' => 0
+//   ],
+//   [
+//     'ospite' => 'Roma',
+//     'casa' => 'Perugia',
+//     'punti_casa'=> 0,
+//     'punti_ospite' => 3
+//   ],
+//   [
+//     'ospite' => 'Torino',
+//     'casa' => 'Napoli',
+//     'punti_casa'=> 3,
+//     'punti_ospite' => 1
+//   ]
+//
+// ];
+
+
+ ?>
+ <!-- <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <ul>
+       <?php// for ($i=0; $i < count($array_matches); $i++) { ?>
+         <?php// $this_match = $array_matches[$i]; ?>
+       <li><?php //echo $this_match['ospite']; ?> - <?php// echo $this_match['casa']; ?> | <?php// echo $this_match['punti_ospite']; ?> - <?php// echo $this_match['punti_casa']; ?></li>
+     <?php //} ?>
+     </ul>
+
+   </body>
+ </html> -->
+
+
+<!-- SNACK N°2 -->
+<?php
+// Passare come parametri GET name, mail e age e verificare
+//  (cercando i metodi che non conosciamo nella documentazione) che:
+// 1. name sia più lungo di 3 caratteri,
+// 2. che mail contenga un punto e una chiocciola
+// 3. e che age sia un numero.
+// Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+if (empty($name) || empty($mail) || empty($age))  {
+  echo 'Accesso Negato';
+}elseif (strlen($name) <= 3) {
+  echo 'Accesso Negato';
+}elseif (strpos($mail, '@') === false || strpos($mail, '.') === false) {
+  echo 'Accesso Negato';
+}elseif (is_numeric($age) === false) {
+  echo 'Accesso Negato';
+}else {
+  echo 'Accesso riuscito';
+}
+?>
